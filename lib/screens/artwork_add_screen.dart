1d1efa569689cc8e99ff3103d9fd9ff44760dfa8
LaFleur <b.turnay@gmail.com>
2025-06-12 09:12:07 +0300

    Tasarım Ödeme ve Siparişler

--- lib/screens/artwork_add_screen.dart
+++ lib/screens/artwork_add_screen.dart
@@ -1,4 +1,3 @@
-// importların başında sadece easy_localization ve diğerler sabit
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
--- lib/screens/offers_screen.dart
+++ lib/screens/offers_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:intl/intl.dart';
 
 import 'chat_screen.dart';
 
